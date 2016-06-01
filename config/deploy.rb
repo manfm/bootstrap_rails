25cc87eb812a0e5788af94750f3d6d86f4dc17f4
Martin Chapcak <martin@odigo.jp>
2016-06-01 17:39:59 +0900

    Basic capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,20 @@
 # config valid only for current version of Capistrano
 lock '3.5.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'bootstrap_rails'
+set :repo_url, 'git@github.com:manfm/bootstrap_rails.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/var/www/bootstrap_rails'
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
+
+# Default value for :log_level is :debug
+set :log_level, :info
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -24,10 +27,10 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -35,8 +38,20 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-namespace :deploy do
+# Set ssh user
+set :user, 'deploy'
+set :ssh_options, {
+  user: fetch(:user)
+}
 
+# For capistrano-puma
+set :puma_init_active_record, true
+
+# For capistrano-bundler
+set :bundle_path, -> { shared_path.join('vendor','bundle') }
+set :bundle_flags, '--deployment'
+
+namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
@@ -45,5 +60,4 @@ namespace :deploy do
       # end
     end
   end
-
 end
